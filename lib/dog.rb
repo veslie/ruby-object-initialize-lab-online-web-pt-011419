003574e8626c3bc096f06510008724d829708b54
veslie losius <veslielosius@yahoo.com>
2019-02-28 21:27:26 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+
+class Dog 
+  attr_accessor :name, :breed 
+  def initialize(name, breed)
+ @name = name
+ @breed = breed
+   end
+ end 
+ class Dog 
+   dog = Dog.new("Fido", "pug")
+  
+ end
+
+   
+  
